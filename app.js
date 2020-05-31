5711fdd40694158896714e9da06e48c697c575f2
coderlidy <448834752@qq.com>
2020-06-01 00:06:29 +0800

    修改样式、完成查询页面

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 //app.js
 App({
+  //数据库参数
+  globalData: {
+    database: 'recite-english-ccz4d',
+    collection: 'words',
+  },
   onLaunch:function(){
-    //小程序端 API 调用的云环境初始化
+    //小程序端 API 调用的云环境初始化 必须有
     wx.cloud.init({
       env: 'recite-english-ccz4d',
     })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,12 @@
 //index.js
 //获取数据库引用.获取表引用
 const words = wx.cloud.database({
-  env: 'recite-english-ccz4d'
-}).collection('words');
-
-
+  env: getApp().globalData.database
+}).collection(getApp().globalData.collection);
+//数据库 API 还提供了一系列的更新指令用于执行更复杂的更新操作，更新指令可以通过 db.command 取得
+const _ = wx.cloud.database({
+  env: getApp().globalData.database
+}).command;
 Page({
   data:{
     words:[{'k':'kkkk','v':'vvvv'},]
@@ -18,7 +20,7 @@ Page({
       data: { "kw": e.detail.value},
       url: 'https://fanyi.baidu.com/sug',
       success: (result) => {
-        console.log(result);
+        //console.log(result);
         if (result.statusCode == 200 && result.data != null) {
           this.setData({
             words:result.data.data
@@ -36,59 +38,69 @@ Page({
   },
   wordClick: function (e) {
     // 通过 currentTarget 获取传递进来的值，前端用data-参数名
-    console.log(e.currentTarget.dataset.item);
-    console.log(new Date());
-    //添加单词到数据库
-    words.add({
-      // data 字段表示需新增的 JSON 数据
-      data: {
-        // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
-        k: e.currentTarget.dataset.item.k,
-        v: e.currentTarget.dataset.item.v,
-        count:0,
-        date: new Date(),
-      },
+    //console.log(e);
+    //先查询数据库看有没有这个单词
+    words.where({
+      k: e.currentTarget.dataset.item.k
+    })
+    .get({
       success: function (res) {
-        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
-        console.log('已添加到生词本', res)
-        wx.showToast({
-          title: "已添加到生词本",
-          icon: 'success',
-          duration: 2000
-        });
-      },
-      fail(res) {
-        console.log('添加失败', res);
-        wx.showToast({
-          title: "添加 " + e.currentTarget.dataset.item.k+" 失败",
-          icon: 'none',
-          duration: 2000
-        });
-      }
-    });
-  },
-
-
-  btnClick: function () {
-    words.add({
-      // data 字段表示需新增的 JSON 数据
-      data: {
-        // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
-        description: "learn cloud database",
-        due: new Date("2018-09-01"),
-        tags: [
-          "cloud",
-          "database"
-        ],
-        done: false
-      },
-      success: function (res) {
-        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
-        console.log('已添加到生词本', res)
-      },
-      fail(res){
-        console.log('添加失败',res);
+        // res.data 是包含以上定义的两条记录的数组
+        if (res.data != null && res.data.length>0){
+          //console.log(res.data[0]._id);
+          //数据库中该单词已经存在，对该id的记录权重+2
+          words.doc(res.data[0]._id).update({
+            data: {
+              // 表示指示数据库将字段自增 2
+              count: _.inc(2)
+            },
+            success: function (res) {
+              wx.showToast({
+                title: "该单词已存在在生词本中！权重+2",
+                icon: 'none',
+                duration: 2000
+              });
+            },
+            fail(res) {
+              wx.showToast({
+                title: "加权失败！",
+                icon: 'none',
+                duration: 2000
+              });
+            }
+          });
+        }else{
+          //添加单词到数据库
+          words.add({
+            // data 字段表示需新增的 JSON 数据
+            data: {
+              // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+              k: e.currentTarget.dataset.item.k,
+              v: e.currentTarget.dataset.item.v,
+              count: 0,
+              date: new Date(),
+            },
+            success: function (res) {
+              // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+              console.log('已添加到生词本', res)
+              wx.showToast({
+                title: "已添加到生词本",
+                icon: 'success',
+                duration: 2000
+              });
+            },
+            fail(res) {
+              console.log('添加失败', res);
+              wx.showToast({
+                title: "添加 " + e.currentTarget.dataset.item.k + " 失败",
+                icon: 'none',
+                duration: 2000
+              });
+            }
+          });
+        };
       }
     })
+    
   },
 })

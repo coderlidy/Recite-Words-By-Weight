302f0a91de1c1a2a3ce9cde0a90650c28458d8dd
coderlidy <448834752@qq.com>
2020-06-01 20:46:26 +0800

    添加弹窗加减权

---
+++ component/row/row.js
@@ -0,0 +1,30 @@
+// component/row/row.js
+Component({
+  /**
+   * 组件的属性列表 对外暴露、可修改
+   */
+  properties: {
+      k:{
+        type:String,
+        value:''
+      },
+      v:{
+        type:String,
+        value:''
+      },
+  },
+
+  /**
+   * 组件的初始数据  对内暴露、可修改
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
--- pages/recite/recite.js
+++ pages/recite/recite.js
@@ -1,11 +1,24 @@
 // pages/recite.js
+//获取数据库引用.获取表引用
+const words = wx.cloud.database({
+  env: getApp().globalData.database
+}).collection(getApp().globalData.collection);
+//数据库 API 还提供了一系列的更新指令用于执行更复杂的更新操作，更新指令可以通过 db.command 取得
+const _ = wx.cloud.database({
+  env: getApp().globalData.database
+}).command;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    words: [{ 'k': 'kkkk', 'v': 'vvvv'},],
+    popwindow:{
+      k:'',
+      v:'',
+      _id:''
+    }
   },
 
   /**
@@ -19,14 +32,85 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
+    //通过id搜索页面组件获得popup组件
+    this.popup = this.selectComponent("#popup");
+    var that=this;
+    words.get({
+      success: function (res) {
+        // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
+        that.setData({
+          words: res.data
+        });
+      }
+    })
+  },
+  wordClick(e){
+    this.setData({
+      popwindow:{
+        k:e.currentTarget.dataset.item.k,
+        v:e.currentTarget.dataset.item.v,
+        _id:e.currentTarget.dataset.item._id
+      }
+    });
+    this.popup.showPopup();
 
   },
 
+  //加权按钮点击后传递到父组件来调用该方法
+  popwindow_inc() {
+    this.popup.hidePopup();
+    //更新数据库，加权
+    words.doc(this.data.popwindow._id).update({
+      data: {
+        // 表示指示数据库将字段自增 1
+        count: _.inc(1)
+      },
+      success: function (res) {
+        wx.showToast({
+          title: "加权成功",
+          icon: 'none',
+          duration: 2000
+        });
+      },
+      fail(res) {
+        wx.showToast({
+          title: "加权失败",
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+  },
+  //减权按钮点击后传递到父组件来调用该方法
+  popwindow_sub() {
+    this.popup.hidePopup();
+    //更新数据库，减权
+    words.doc(this.data.popwindow._id).update({
+      data: {
+        // 表示指示数据库将字段自增 -1
+        count: _.inc(-1)
+      },
+      success: function (res) {
+        wx.showToast({
+          title: "减权成功",
+          icon: 'none',
+          duration: 2000
+        });
+      },
+      fail(res) {
+        wx.showToast({
+          title: "减权失败",
+          icon: 'none',
+          duration: 2000
+        });
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    
   },
 
   /**
